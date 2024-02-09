71fcc9f97bb917e3e83f31d952dedd788b0bbc22
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-02-09 13:34:00 -0800

    First update HashTable.h

--- HashTable.h
+++ HashTable.h
@@ -1,18 +1,21 @@
-#pragma once
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
 #include <vector> 
 #include "Student.h"
 
 class HashTable { 
     private: 
+        static const int INTIAL_SIZE = 10; 
         std::vector<std::vector<Student*>> table; 
         int size; 
-        int hasFunction(int id); 
+        int hashFunction(int id); 
         void resizeTable(); 
-        void rehash(); 
 
     public: 
-        HashTable(int initialSize); 
-        void addStudent(Student* newStudent); 
-        void printStudents(); 
-        void deleteStudent(int id); 
-}; 
+        HashTable();
+        ~HashTable(); 
+        void addStudent(Student* student); 
+        void printStudents() const; 
+};
+
+#endif
