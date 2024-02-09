1bf64328650023cddd56c30d8e89abc73437f5b9
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-02-09 13:49:53 -0800

    Update  2 - HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -2,7 +2,7 @@
 #include <iostream> 
 
 HashTable::HashTable() : size(INTIAL_SIZE) { 
-    table.resizeTable(INTIAL_SIZE); 
+    table.resize(INTIAL_SIZE); 
 }
 
 HashTable::~HashTable() { 
@@ -14,15 +14,23 @@ HashTable::~HashTable() {
 }
 
 void HashTable::addStudent(Student* student) { 
-    int index = hashFunction(student-> getID()); 
+    int index = hashFunction(student->getID()); 
     table[index].push_back(student); 
 }
 
 void HashTable::printStudent() const { 
-    for (const autp& chain : table) { 
+    for (const auto& chain : table) { 
         for (auto student : chain) { 
-            std::cout << "ID: " << student->getID() << ", Name: " << student -> getFirstName() << " " << student -> getLastName() << 
-        
+            std::cout << "ID: " << student->getID() << ", Name: " << student->getFirstName() << student-> getLastName() << ", GPA: " << student->getGPA() << std::endl; 
         }
     }
+}
+
+int HashTable::hashFunction(int id) { 
+    return id % size;
+}
+
+void HashTable::resizeTable() { 
+    size *= 2; 
+    table.resize(size); 
 }
