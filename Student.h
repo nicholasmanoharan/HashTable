9cca210e6d5265a152e8b53103622d2796184138
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-02-09 13:35:04 -0800

    First ipdate Student.h

--- Student.h
+++ Student.h
@@ -1,12 +1,20 @@
-#include once
+#ifndef STUDENT_H
+#define STUDENT_H
 #include <string> 
 
 class Student { 
-    public:     
+    private: 
         int id; 
         std::string firstName; 
-        std::string lastName;
-        float gpa; 
+        std::string lastName; 
+        float gpa;
 
-        Student(int id, const std::string& firstName, const std::string& lastName, float gpa);
-}
+    public: 
+        Student(int id, const std::string& firstName, const std::string& lastNamem, float gpa);
+        int getID() const; 
+        std::string getFirstName() const; 
+        std::string getLastName() const; 
+        float getGPA() const; 
+};
+
+#endif
