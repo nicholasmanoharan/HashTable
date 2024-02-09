e9a03aba8ed7ae81db71b5141ea9e05df42751d3
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-02-09 13:34:41 -0800

    First update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,3 +1,19 @@
 #include "Student.h"
 
 Student::Student(int id, const std::string& firstName, const std::string& lastName, float gpa) : id(id), firstName(firstName), lastName(lastName), gpa(gpa) {}
+
+int Student::getID() const { 
+    return id; 
+}
+
+std::string Student::getFirstName() const { 
+    return firstName;
+}
+
+std::string Student::getLastName() const { 
+    return lastName; 
+}
+
+float Student::getGPA() const { 
+    return gpa; 
+}
