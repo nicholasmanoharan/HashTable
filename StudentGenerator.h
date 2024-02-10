4e34bdfd9e5c1223dd72f9f7ec12d8890b47b43e
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-02-09 22:59:24 -0800

    StudentGenerator.h final code

--- StudentGenerator.h
+++ StudentGenerator.h
@@ -1,10 +1,22 @@
-#ifndef STUDENT_GENERATOR_H
-#define STUDENT_GENERATOR_H
-#include "Student.h"
+#ifndef STUDENTGENERATOR_H
+#define STUDENTGENERATOR_H
 
-class StudentGenerator { 
-    public: 
-        static Student* generateStudent();
+#include <string>
+#include <vector>
+#include "HashTable.h" 
+
+class StudentGenerator {
+private:
+    std::vector<std::string> firstNames;
+    std::vector<std::string> lastNames;
+
+    std::string getRandomFirstName() const;
+    std::string getRandomLastName() const;
+    double generateRandomGPA() const;
+
+public:
+    StudentGenerator(const std::string& firstNamesFile, const std::string& lastNamesFile);
+    void generateAndAddStudents(int numStudents, HashTable& table);
 };
 
 #endif
