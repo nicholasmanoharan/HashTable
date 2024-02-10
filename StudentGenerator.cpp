5d9860bdfce3a9eb7b6a1b9d2fd13a6a25bcd55f
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-02-09 22:59:01 -0800

    StudentGenerator.cpp final code

--- StudentGenerator.cpp
+++ StudentGenerator.cpp
@@ -1,11 +1,48 @@
 #include "StudentGenerator.h"
-#include <iostream> 
-#include <cstdlib> 
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
 
-Student* StudentGenerator::generateStudent() { 
-    int id = rand() % 1000 + 1; 
-    std::string firstName = "First Name"; 
-    std::string lastName = "Last Name";
-    float gpa = static_cast<float>(rand()) / RAND_MAX * 4.0f; 
-    return new Student(id, firstName, lastName, gpa);
+StudentGenerator::StudentGenerator(const std::string& firstNamesFile, const std::string& lastNamesFile) {
+
+    std::ifstream firstNameStream(firstNamesFile);
+    std::string firstName;
+    while (std::getline(firstNameStream, firstName)) {
+        firstNames.push_back(firstName);
+    }
+    firstNameStream.close();
+
+
+    std::ifstream lastNameStream(lastNamesFile);
+    std::string lastName;
+    while (std::getline(lastNameStream, lastName)) {
+        lastNames.push_back(lastName);
+    }
+    lastNameStream.close();
+
+    
+    std::srand(std::time(nullptr));
+}
+
+std::string StudentGenerator::getRandomFirstName() const {
+    int index = std::rand() % firstNames.size();
+    return firstNames[index];
+}
+
+std::string StudentGenerator::getRandomLastName() const {
+    int index = std::rand() % lastNames.size();
+    return lastNames[index];
+}
+
+double StudentGenerator::generateRandomGPA() const {
+    return (std::rand() % 401) / 100.0; 
+}
+
+void StudentGenerator::generateAndAddStudents(int numStudents, HashTable& table) {
+    for (int i = 0; i < numStudents; ++i) {
+        std::string firstName = getRandomFirstName();
+        std::string lastName = getRandomLastName();
+        double GPA = generateRandomGPA();
+        table.addStudent(firstName, lastName, GPA);
+    }
 }
